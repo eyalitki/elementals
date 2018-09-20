1fdc76c2cbcc3cf164a76cb14e11f491592717ea
eyalitki <eyal.itkin@hotmail.com>
2018-09-20 22:19:36 +0900

    Updated setup.py to include description

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+from codecs     import open
+from os         import path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='elementals',
-      version='1.0',
-      description='Basic utilities for CTF scripts',
+      version='1.1.1',
+      description='Basic utilities for CTF (or exploit) scripts',
       author='Eyal Itkin',
       author_email='eyal.itkin@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/eyalitki/elementals',
       license='GPL',
       packages=find_packages(exclude=['tests']),
       install_requires=['colorama'],
+      classifiers=[
+                    "Programming Language :: Python",
+                    "License :: OSI Approved :: GNU General Public License (GPL)",
+                    "Operating System :: OS Independent",
+                  ],
       zip_safe=False)
