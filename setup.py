d805ce39d3e47b6f728027f0c1d0457c901b9f5b
Eyal Itkin <eyal.itkin@hotmail.com>
2018-12-14 09:57:32 +0200

    Better PyPi description: fro the next release

--- setup.py
+++ setup.py
@@ -2,8 +2,38 @@ from setuptools import setup, find_packages
 from codecs     import open
 from os         import path
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# Different from the README.md, because PyPi struggles with shwoing PNG images
+long_description = '''# elementals
+Python package with basic utilities for CTF scripts (works well for exploit PoCs too).
+
+## Install
+```pip install elementals```
+
+## Read The Docs
+https://elementals.readthedocs.io/
+
+## Github
+https://github.com/eyalitki/elementals
+
+## Brief
+The **elementals** python package is a utility package with handy tools for CTF scripts and PoC-style scripts. What was first implemented for personal use in small research scripts was now upgraded to be used by security researchers as a lightweight substitute to the famous pwntools package.
+
+The features included in **elementals** are:
+* **Logger:** Basic (logging based) logger - configured and ready to use 
+* **Prompter:** Metasploit based stdout wrapper for the logger
+* **ProgressBar:** User-friendly graphical progress bar
+* **StatusBar:** User-friendly graphical status bar
+* **createAnchor:** Creates a time-stamped output directory for all script outputs
+* **hexDump:** Stylized hex dump for binary blobs / strings
+
+## Used by
+The **elementals** package is used in most of my scripts and also in my public Github projects:
+* **Scout Debugger** - https://github.com/CheckPointSW/Scout
+* **Karta (IDA Plugin)** - (soon to be released)
+
+## References
+* Twitter: [@EyalItkin](https://twitter.com/EyalItkin)
+* E-mail: eyal dot itkin at gmail dot com'''
 
 setup(name='elementals',
       version='1.2.2',
