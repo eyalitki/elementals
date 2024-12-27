ac938a4da3791978f2502eb2aea4fb9eb0083cab
Eyal Itkin <eyal.itkin@gmail.com>
2024-12-27 16:10:02 +0200

    [version] Version bump to 1.3.3
    Release Notes:
    * Minor bug fix for file logger when prompter is used with additional
      input coloring.
    
    Signed-off-by: Eyal Itkin <eyal.itkin@gmail.com>

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ The **elementals** package is used in most of my scripts and also in my public G
 * E-mail: eyal dot itkin at gmail dot com'''
 
 setup(name='elementals',
-      version='1.3.2',
+      version='1.3.3',
       description='Basic utilities for CTF (or exploit) scripts',
       author='Eyal Itkin',
       author_email='eyal.itkin@gmail.com',
