c5ac4bbebc20bc9ec7ade7528130fa6862a364c4
Eyal Itkin <eyal.itkin@gmail.com>
2026-07-17 20:07:05 +0300

    Version bump to 1.4.3
    Release Notes:
    * Bug fix for hang on macOS - switch to native color support
    * Docs update
    
    Signed-off-by: Eyal Itkin <eyal.itkin@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Eyal Itkin'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.4.2'
+release = u'1.4.3'
 
 
 # -- General configuration ---------------------------------------------------
